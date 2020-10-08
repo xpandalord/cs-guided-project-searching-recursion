16cab4af3fc94c84c50229dab4e449b95b9aada7
Andy Tran <xpandalord@gmail.com>
2020-10-08 18:52:53 -0400

    updates

--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -4,26 +4,58 @@ series of drafts. Each draft is based on the previous draft. The latest draft
 of your book has a serious typo. Since each newer draft is based on the
 previous draft, all the drafts after the draft containing the typo also include
 the typo.
-
+​
 Suppose you have `n` drafts `[1, 2, 3, ..., n]` and you need to find out the
 first one containing the typo (which causes all the following drafts to have
 the typo as well).
-
+​
 You are given access to an API tool `containsTypo(draft)` that will return
 `True` if the draft contains a typo and `False` if it does not.
-
+​
 You need to implement a function that will find the *first draft that contains
 a typo*. Also, you have to pay a fee for every call to `containsTypo()`, so
 make sure that your solution minimizes the number of API calls.
-
+​
 Example:
-
+​
 Given `n = 5`, and `draft = 4` is the first draft containing a typo.
-
+​
 containsTypo(3) -> False
 containsTypo(5) -> True
 containsTypo(4) -> True
 """
+​
 def firstDraftWithTypo(n):
-    # Your code here
-
+    left = 1
+    right = n
+​
+    while left < right:
+        # figure out out the midpoint of the range 1 .. n 
+        mid = left + (right - left) // 2
+        # check if the midpoint draft has a typo 
+        # if it has a typo
+        if containsTypo(mid):
+            # this is either the first draft typo, or 
+            # it happened earlier 
+            # so we need check the earlier drafts to 
+            # determine where the first typo is 
+            right = mid
+        # it doesn't have a typo 
+        else:
+            # the typo must have happened in a later draft 
+            # so we don't have to consider any of the earlier drafts
+            left = mid + 1 
+            
+    return left 
+​
+​
+# def firstDraftWithTypo(n):
+#     # Your code here
+#     # iterate through from 1 to n 
+#     for i in range(1, n + 1): # O(n)
+#         # if containsTypo(draft)
+#         if containsTypo(i): # O(1)
+#             # return draft 
+#             return i
+#     # return -1
+#     return -1 
--- src/demonstration_3.py
+++ src/demonstration_3.py
@@ -4,24 +4,34 @@ jar of cookies with `n` cookies inside of it, how many ways could he eat all
 `n` cookies in the cookie jar? Implement a function `eating_cookies` that
 counts the number of possible ways Cookie Monster can eat all of the cookies in
 the jar.
-
+​
 For example, for a jar of cookies with n = 3 (the jar has 3 cookies inside it),
 there are 4 possible ways for Cookie Monster to eat all the cookies inside it:
-
+​
 He can eat 1 cookie at a time 3 times
 He can eat 1 cookie, then 2 cookies
 He can eat 2 cookies, then 1 cookie
 He can eat 3 cookies all at once.
 Thus, eating_cookies(3) should return an answer of 4.
-
-Can you implement a solution that has a O(n) time complexity and a O(n) space
-complexity?
-
+​
 *Note: Since this question is asking you to generate a bunch of possible
 permutations, you'll probably want to use recursion for this. Think about base
 cases that we would want our recursive function to stop recursing on (when do
 you know you've found a "way" to eat the cookies versus when you have not?).*
 """
-def eating_cookies(n, cache = None):
+def eating_cookies(n):
     # Your code here
-
+    # what are our base cases? 
+    # if n < 0, then this route didn't contribute anything 
+    if n < 0:
+        return 0
+    # if n == 0, then this route was valid, and we'll return 1 
+    if n == 0:
+        return 1 
+    # how do we move towards one of these base cases? 
+    # return eating_cookies(n - 1) # represents eating 1 cookie 
+    #  + eating_cookies(n - 2) # represents eating 2 cookies 
+    #  + eating_cookies(n - 3) # represents eating 3 cookies
+    return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3) 
+​
+print(eating_cookies(50))
